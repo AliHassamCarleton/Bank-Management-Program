357c059861fec96e1c205d9a412cb705eea9148b
AliHassamCarleton <alihassam@cmail.carleton.ca>
2016-10-07 15:58:00 -0400

    Add files via upload

---
+++ CustArray.cc
@@ -0,0 +1,54 @@
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+#include "CustArray.h"
+
+
+CustArray::CustArray()
+{
+  size = 0;
+}
+
+int CustArray::getSize() { return size; }
+
+Customer* CustArray::get(int index)
+{
+  if (index < 0 || index >= size)
+    exit(1);
+  return elements[index];
+}
+
+void CustArray::add(Customer* cust)
+{
+  if (size >= MAX_ARR)
+    return;
+
+  elements[size++] = cust;
+}
+
+//returns true or false depending on if the customer is existing or not
+bool CustArray::isExisting(int custNum){
+
+	for (int i=0; i<size; i++){
+
+		if(custNum==elements[i].getCustId()){
+		  return true;
+		}
+
+	}
+	return false;
+}
+
+
+//takes in id and returns the customers name
+string CustArray::idtoName(int custNum){
+
+  for (int i=0; i<size; i++){
+
+    if(custNum==elements[i].getCustId()){
+      return elements[i].getCustName();
+    }
+  }
+  return " ";
+}
---
+++ Customer.cc
@@ -0,0 +1,14 @@
+#include "defs.h"
+#include "Customer.h"
+using namespace std;
+
+
+Customer::Customer(int id, string n){
+
+	custId= id;
+	name= n;
+
+}
+
+string   Customer::getCustName()  { return name;  }
+int      Customer::getCustId()    { return custId;    }
