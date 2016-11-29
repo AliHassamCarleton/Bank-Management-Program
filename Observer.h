49fc864fe26c9adf2dbfc059f553556d1f3eb721
AliHassamCarleton <alihassam@cmail.carleton.ca>
2016-11-29 17:14:38 -0500

    Add files via upload

---
+++ Observer.h
@@ -0,0 +1,29 @@
+using namespace std;
+#ifndef OBSERVER_H
+#define OBSERVER_H
+
+#include "defs.h"
+
+class Obersver
+{
+
+	public:
+		virtual bool update(float)  = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
+
+#endif
